0829d27704c9c56e1207c99e04ea4daa1dc8c4af
Fendo181 <fendo181git@gmail.com>
2018-05-22 08:02:20 +0900

    OS X以外のOSで閉じられた際の挙動を統一した。

--- MyApp/index.js
+++ MyApp/index.js
@@ -9,8 +9,8 @@ const BrowserWindow = electron.BrowserWindow;
 // ブロックスコープの局所変数を宣言
 let mainWindow;
 
-// 開かれた際の処理
-app.on('ready', function(){
+
+function createMainWinowd(){
     // create window
     mainWindow = new BrowserWindow({width: 600, height:400 });
     mainWindow.loadURL('file://'+ __dirname + '/index.html');
@@ -20,4 +20,24 @@ app.on('ready', function(){
     mainWindow.on('closed', function(){
         mainWindow = null;
     });
+}
+
+// 開かれた際の処理
+app.on('ready', function(){
+    createMainWinowd();
+});
+
+app.on('window-all-closed', function(){
+    //macユーザ以外の処理
+    if(process.platform !== 'darein'){
+        app.quit();
+    }
+});
+
+// ウィンドウが閉じられたけれど
+// Dock アイコンや Spotlight から再度アプリが呼び出されたときにウィンドウを作る処理
+app.on('activate',function(){
+    if(mainWindow  === null){
+      createMainWinowd();
+    }
 });
